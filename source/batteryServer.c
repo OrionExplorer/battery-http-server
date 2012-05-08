7516ee37650e191aa27bb1dabb681f58e901b901
Marcin Kelar <marcin.kelar@gmail.com>
2012-05-08 12:27:41 +0300

    Reinstated app_terminate function.

--- source/batteryServer.c
+++ source/batteryServer.c
@@ -15,21 +15,21 @@ Autor: Marcin Kelar (marcin.kelar@holicon.pl)
 #include "include/server_socket_io.h"
 #include <signal.h>
 
-//void app_terminate(void);
+void app_terminate(void);
 
 int main(void)
 {
-//	signal(SIGABRT, &app_terminate);
-//	signal(SIGTERM, &app_terminate);
-//	signal(SIGINT, &app_terminate);
+	signal(SIGABRT, (__sighandler_t)&app_terminate);
+	signal(SIGTERM, (__sighandler_t)&app_terminate);
+	signal(SIGINT, (__sighandler_t)&app_terminate);
 	server_initialize();
 	return 0;
 }
 
-//void app_terminate(void)
-//{
-//	print_log("Server is being closed...\n");
-//	socket_free();
-//	print_log("Server closed.\n");
-//	log_save();
-//}
+void app_terminate(void)
+{
+	print_log("Server is being closed...\n");
+	socket_free();
+	print_log("Server closed.\n");
+	log_save();
+}
